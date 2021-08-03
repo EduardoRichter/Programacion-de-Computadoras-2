fbe0980149d228c9a68d2e57ad2905724bcf2761
Eduardo <82736316+EduardoRichter@users.noreply.github.com>
2021-08-03 15:51:40 -0300

    correccion de indentado

--- Ejercicio.c
+++ Ejercicio.c
@@ -12,32 +12,29 @@ valores e imprimirlos ordenados
 
 void enterosOrdenados(int *entero1, int *entero2);
 
-int main(){
+int main()
+{
 
     int entero1, entero2;
     printf("Ingrese un numero ");
-    scanf("%d",&entero1);
+    scanf("%d", &entero1);
     printf("Ingrese otro numero ");
-    scanf("%d",&entero2);
+    scanf("%d", &entero2);
 
     enterosOrdenados(&entero1, &entero2);
 
     printf("El menor de los numeros es %i y el mayor de los numeros es % i\n", entero1, entero2);
 
-
-
     return 0;
 }
 
-void enterosOrdenados(int *entero1, int *entero2){
+void enterosOrdenados(int *entero1, int *entero2)
+{
 
-      if (*entero1 > *entero2)
-      {
-         int a= *entero1;
-         *entero1 = *entero2;
-         *entero2 = a;
-         
-      }
-      
-
+    if (*entero1 > *entero2)
+    {
+        int a = *entero1;
+        *entero1 = *entero2;
+        *entero2 = a;
+    }
 }
